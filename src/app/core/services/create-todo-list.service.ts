2f5c8879a8e52d5044e89083e112868e2ae8f579
Andrew Miller <andrew@advaitabio.com>
2022-10-03 11:37:18 -0400

    Added create todo list action
    📅  | Mon Oct  3 11:41:21 EDT 2022
    
    📅  | Mon Oct  3 11:42:42 EDT 2022
    
    📅  | Mon Oct  3 11:43:30 EDT 2022
    
    📅  | Mon Oct  3 11:44:00 EDT 2022

--- src/app/core/services/create-todo-list.service.ts
+++ src/app/core/services/create-todo-list.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { TodoList } from '../types/todo-list.types';
+import { TodoListStateService } from './todo-list-state.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CreateTodoListService {
-  constructor() {}
+  constructor(public todoListStateService: TodoListStateService) {}
   addTodoList(title: string) {
-    console.log(title);
+    const newTodoList: TodoList = {
+      id: this.createTodoListId(),
+      todoListTitle: title,
+      todos: [],
+    };
+    this.todoListStateService.todoLists.push(newTodoList);
+  }
+
+  createTodoListId() {
+    return Date.now() + Math.random() * 100;
   }
 }
